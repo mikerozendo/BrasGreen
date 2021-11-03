caa884766756437d7475291d988a694f8f8a49c5
mikerozendo <mikerozendo@gmail.com>
2021-11-03 20:12:19 -0300

    Adicionando entidades

---
+++ BrasGreen.Domain/Entities/Documento.cs
@@ -0,0 +1,10 @@
+﻿namespace BrasGreen.Domain.Entities
+{
+    public class Documento
+    {
+        public int DocumentoID { get; set; }
+        public string CPF { get; set; }
+        public string CNPJ { get; set; }
+        public string RG { get; set; }
+    }
+}
---
+++ BrasGreen.Domain/Entities/PessoaFisica.cs
@@ -0,0 +1,15 @@
+﻿using BrasGreen.Domain.Enums;
+using System;
+
+namespace BrasGreen.Domain.Entities
+{
+    public class PessoaFisica
+    {
+        public int PessoaFisicaID { get; set; }
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public Documento Documentacao { get; set; }
+        public EnumTipoPessoa EnumTipoPessoa { get; set; }
+    }
+}
