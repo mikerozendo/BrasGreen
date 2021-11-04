d59614a47a8bcab152b8634453d2068df5385e23
mikerozendo <mikerozendo@gmail.com>
2021-11-04 18:49:28 -0300

    Bibliotecas

---
+++ BrasGreen.Domain/Enums/EnumTipoPagamento.cs
@@ -0,0 +1,12 @@
+﻿namespace BrasGreen.Domain.Enums
+{
+    public enum EnumTipoPagamento
+    {
+        Boleto = 1,
+        Dinheiro = 2,
+        DepósitoBancário = 3,
+        CartãoCredito = 4,
+        CartãoDebito = 5,
+        Pix = 6
+    }
+}
--- BrasGreen.Repository/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace BrasGreen.Repository
-{
-    public class Class1
-    {
-    }
-}
