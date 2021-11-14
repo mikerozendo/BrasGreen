0d5932b2c07f24d96e959bab5f9aafb8ac355cdd
mikerozendo <mikerozendo@gmail.com>
2021-11-14 01:47:08 -0300

    Adicionando atributo cnpjraiz

--- BrasGreen.Domain/Entities/Documento.cs
+++ BrasGreen.Domain/Entities/Documento.cs
@@ -5,6 +5,7 @@
         public int DocumentoID { get; set; }
         public string CPF { get; set; }
         public string CNPJ { get; set; }
+        public string CnpjRaiz { get; set; }
         public string RG { get; set; }
         public string InscricaoEstadual { get; set; }
     }
