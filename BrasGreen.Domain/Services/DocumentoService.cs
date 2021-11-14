7cc612fd6fba796f76ea814efd9c11df48fde3a1
mikerozendo <mikerozendo@gmail.com>
2021-11-13 22:14:56 -0300

    Finalizando calculo DV

--- BrasGreen.Domain/Services/DocumentoService.cs
+++ BrasGreen.Domain/Services/DocumentoService.cs
@@ -14,36 +14,53 @@ namespace BrasGreen.Domain.Services
             char[] caracteres = FormatarRG(rg).ToCharArray();
             int resultadoDv = 0;
 
-            //Alterar retornos quando implementar DVs X e 0
+            int somaDv = 0;
+            Dictionary<int, int> dictionary = new Dictionary<int, int>();
+
+            try
+            {
+                for (int i = 0; i < caracteres.Length - 1; i++)
+                {
+                    dictionary.Add(i, (i + 2) * int.Parse(caracteres[i].ToString()));
+                    if (dictionary.TryGetValue(i, out int value))
+                    {
+                        somaDv += value;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+
             if (caracteres.Last() != 'X' && caracteres.Last() != '0')
             {
-                int somaDv = 0;
-                Dictionary<int, int> dictionary = new Dictionary<int, int>();
-                try
+                resultadoDv = (somaDv % 11) - 11;
+
+                if (resultadoDv.ToString() == caracteres.Last().ToString())
                 {
-                    for (int i = 0; i < caracteres.Length - 1; i++)
-                    {
-                        dictionary.Add(i, (i + 2) * int.Parse(caracteres[i].ToString()));
-                        if (dictionary.TryGetValue(i, out int value))
-                        {
-                            somaDv += value;
-                        }
-                    }
-
-                    resultadoDv = (somaDv % 11) - 11;
-
-                    if (resultadoDv.ToString() == caracteres.Last().ToString())
-                    {
-                        return true;
-                    }
+                    return true;
                 }
-                catch (Exception ex)
-                {
-
-                    throw new Exception(ex.Message);
-                }
-               
             }
+            else if (caracteres.Last() == 'X')
+            {
+                resultadoDv = (somaDv % 11);
+
+                if (resultadoDv.ToString() == caracteres.Last().ToString() && resultadoDv == 10)
+                {
+                    return true;
+                }
+            }
+            else if (caracteres.Last() == '0')
+            {
+                resultadoDv = (somaDv % 11);
+
+                if (resultadoDv.ToString() == caracteres.Last().ToString() && resultadoDv == 11)
+                {
+                    return true;
+                }
+            }
+
             return false;
         }
 
